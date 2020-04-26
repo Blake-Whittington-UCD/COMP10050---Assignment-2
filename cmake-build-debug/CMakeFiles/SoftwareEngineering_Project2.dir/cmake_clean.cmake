2893efbfddd3d7e00a13b39a97b04ae88be22719
Blake-Whittington-UCD <blake.whittington@ucdconnect.ie>
2020-04-26 18:36:09 +0000

    Second commit of main.c

---
+++ cmake-build-debug/CMakeFiles/SoftwareEngineering_Project2.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SoftwareEngineering_Project2.dir/game_init.c.obj"
+  "CMakeFiles/SoftwareEngineering_Project2.dir/input_output.c.obj"
+  "CMakeFiles/SoftwareEngineering_Project2.dir/main.c.obj"
+  "CMakeFiles/SoftwareEngineering_Project2.dir/stack.c.obj"
+  "SoftwareEngineering_Project2.exe"
+  "SoftwareEngineering_Project2.exe.manifest"
+  "SoftwareEngineering_Project2.pdb"
+  "libSoftwareEngineering_Project2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SoftwareEngineering_Project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
